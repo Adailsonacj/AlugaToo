a201b1e38da92c72d4d9ee9e3b042d8dd9abfba8
Adailsonacj <adailsonacj@live.com>
2018-03-13 11:04:44 -0300

    Commit 13/03/2018 11:04

---
+++ ValidaLogin.php
@@ -0,0 +1,27 @@
+<?php include("./database/Conexao.php") ?>
+<?php
+
+session_start();
+$login = $_POST['login'];
+$senha = $_POST['senha'];
+//$dao = new DaoUsuario();
+
+$conexao = new Conexao();
+$instance = $conexao::conecta();
+echo $sql = "select * from usuario where email = '{$login}' AND senha = '{$senha}'";
+$stmt = $instance->prepare($sql);
+$stmt->execute();
+
+Conexao::desconecta();
+
+if ($stmt->rowCount() > 0) {
+    $_SESSION['login'] = $login;
+    $_SESSION['senha'] = $senha;
+    header("location:index.php");
+} else {
+    unset($_SESSION['login']);
+    unset($_SESSION['senha']);
+    header("location:login.php");
+
+    
+}
---
+++ daos/DaoUsuario.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adailson
+ * Date: 13/03/18
+ * Time: 10:12
+ */
